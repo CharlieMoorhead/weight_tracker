4130d1b20560c5e612ee5963b845152627229125
Charlie <camoorhead@gmail.com>
2012-04-05 17:20:10 -0400

    minor changes to header and css, removed unnecessary migrations

--- db/migrate/20120402021630_remove_workout_id_from_exercise.rb
+++
@@ -1,9 +0,0 @@
-class RemoveWorkoutIdFromExercise < ActiveRecord::Migration
-  def self.up
-    remove_column :exercises, :workout_id
-  end
-
-  def self.down
-    add_column :exercises, :workout_id, :integer
-  end
-end
