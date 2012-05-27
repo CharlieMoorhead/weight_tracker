207f0ac5a529ddffffc1946e5d066903d38f6bc4
Charlie <camoorhead@gmail.com>
2012-05-27 02:40:24 -0400

    added production settings for user_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ WeightTracker::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  ActionMailer::Base.default_url_options[:host] = "young-sword-6555.heroku"
+  
 end
