315a6e4b4c373116fec8f857868269563754a153
Charlie <camoorhead@gmail.com>
2012-05-26 22:55:30 -0400

    fixed the mail config

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,10 +5,10 @@ ActionMailer::Base.smtp_settings = {
   :address               => "smtp.gmail.com",
   :port                  => 587,
   :domain                => "gmail.com",
-  :user_name             => "[REDACTED]",
-  :password              => "[REDACTED]",
+  :user_name             => "charlie.email.robot@gmail.com",
+  :password              => "Gorilla Cheese Horse Sandwich",
   :authentication        => "plain",
   :enable_starttls_auto  => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "[REDACTED]"
+ActionMailer::Base.default_url_options[:host] = "localhost:3000"
